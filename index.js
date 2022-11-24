dfc4ece4497c763b6d2db586ba979a4f0e5181e9
MIEBKA LONGJOHN <miebakalongjohn@gmail.com>
2022-11-24 13:49:59 +0100

    new js added

---
+++ index.js
@@ -0,0 +1,19 @@
+let startBtn = document.getElementById('start');
+
+let pauseBtn = document.getElementById('pause');
+
+let resetBtn = document.getElementById('reset');
+
+
+
+function start(){
+    console.log('yo');
+}
+
+function pause(){
+    console.log('hi');
+}
+
+function reset(){
+    console.log('hey');
+}

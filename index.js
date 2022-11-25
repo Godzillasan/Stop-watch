df36475c22fce73e7b50299c062884a4f53b0f1a
MIEBKA LONGJOHN <miebakalongjohn@gmail.com>
2022-11-25 13:19:13 +0100

    start implementing javascript

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-let startBtn = document.getElementById('start');
+let  seconds = 00;
 
-let pauseBtn = document.getElementById('pause');
+let minutes = 00;
 
-let resetBtn = document.getElementById('reset');
+let hours = 00;
 
+let increaseSecond =document.getElementById("seconds");
 
+let increaseMinutes =document.getElementById("minutes");
 
-function start(){
-    console.log('yo');
+let increaseHours =document.getElementById("hours");
+
+let startButton = document.getElementById("start");
+
+let stopButton = document.getElementById("pause");
+
+let resetButton = document.getElementById("restart");
+
+function startTimer(){
+   seconds++;
+   if (seconds < 0) {
+    increaseSecond.innerHTML = "0" + seconds;
+   }
+   if (seconds > 0) {
+    increaseSecond.innerHTML = seconds;
+   }
+   if (seconds > 60) {
+    increaseSecond.innerHTML = "0" + seconds;
+    seconds = 0;
+    increaseSecond.innerHTML = "0" + 0;
+   }
 }
 
-function pause(){
-    console.log('hi');
-}
-
-function reset(){
-    console.log('hey');
-}
+// startTimer();
